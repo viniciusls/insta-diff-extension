95f7d9f010afcd4d146c708b68e73dc5ba6d0b5d
Vinicius Silva <vinicius.silva@ibm.com>
2021-03-01 00:25:12 -0300

    Add bootstrap, icon, logo, manifest.json and main HTML/JS files

---
+++ popup.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+}, false);
+
+function getDiff() {
+  alert(`Get Diff called!`);
+}
+
+function getFollowers() {
+  alert(`Get Followers called!`)
+}
+
+function getFollowings() {
+  alert(`Get Followings called!`)
+}
